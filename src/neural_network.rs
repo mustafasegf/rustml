f94f963b32994a00b6b4d3ab1a2087a47b0d3ab8
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-06-28 22:55:42 +0700

    test: added test for neural network

--- src/neural_network.rs
+++ src/neural_network.rs
@@ -160,3 +160,186 @@ impl std::fmt::Display for NeuralNetwork {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let nn = NeuralNetwork::new(&[2, 3, 1]);
+        assert_eq!(nn.size, 2);
+        assert_eq!(nn.weight.len(), 2);
+        assert_eq!(nn.bias.len(), 2);
+        assert_eq!(nn.activation.len(), 3);
+        assert_eq!(nn.activation[0], Matrix::new(1, 2));
+        assert_eq!(nn.activation[1], Matrix::new(1, 3));
+        assert_eq!(nn.activation[2], Matrix::new(1, 1));
+        assert_eq!(nn.weight[0], Matrix::new(2, 3));
+        assert_eq!(nn.weight[1], Matrix::new(3, 1));
+        assert_eq!(nn.bias[0], Matrix::new(1, 3));
+        assert_eq!(nn.bias[1], Matrix::new(1, 1));
+    }
+
+    #[test]
+    fn test_new_random() {
+        let nn = NeuralNetwork::new_random(&[2, 3, 1]);
+        assert_eq!(nn.size, 2);
+        assert_eq!(nn.weight.len(), 2);
+        assert_eq!(nn.bias.len(), 2);
+        assert_eq!(nn.activation.len(), 3);
+        assert_eq!(nn.activation[1].rows(), 1);
+        assert_eq!(nn.activation[1].cols(), 3);
+        assert_eq!(nn.activation[2].rows(), 1);
+        assert_eq!(nn.activation[2].cols(), 1);
+        assert!(nn.weight[0].iter().any(|&x| x != 0.0));
+        assert!(nn.weight[1].iter().any(|&x| x != 0.0));
+        assert!(nn.bias[0].iter().any(|&x| x != 0.0));
+        assert!(nn.bias[1].iter().any(|&x| x != 0.0));
+    }
+
+    #[test]
+    fn test_forward() {
+        let mut nn = NeuralNetwork::new(&[2, 3, 1]);
+        nn.weight[0] = Matrix::from_iter(2, 3, vec![0.1, 0.2, 0.3, 0.4, 0.5, 0.6]);
+        nn.weight[1] = Matrix::from_iter(3, 1, vec![0.7, 0.8, 0.9]);
+
+        nn.bias[0] = Matrix::from_iter(1, 3, vec![0.1, 0.2, 0.3]);
+        nn.bias[1] = Matrix::from_iter(1, 1, vec![0.4]);
+        nn.activation[0] = Matrix::from_iter(1, 2, vec![0.5, 0.6]);
+
+        nn.forward();
+
+        assert_eq!(
+            nn.activation[1],
+            Matrix::from_iter(
+                1,
+                3,
+                vec![0.5962826992967879, 0.6456563062257954, 0.6921095043017882]
+            )
+        );
+        assert_eq!(
+            nn.activation[2],
+            Matrix::from_iter(1, 1, vec![0.8761885526812198])
+        );
+    }
+
+    #[test]
+    fn test_cost() {
+        let mut nn = NeuralNetwork::new(&[2, 3, 1]);
+        nn.weight[0] = Matrix::from_iter(2, 3, vec![0.1, 0.2, 0.3, 0.4, 0.5, 0.6]);
+        nn.weight[1] = Matrix::from_iter(3, 1, vec![0.7, 0.8, 0.9]);
+        nn.bias[0] = Matrix::from_iter(1, 3, vec![0.1, 0.2, 0.3]);
+        nn.bias[1] = Matrix::from_iter(1, 1, vec![0.4]);
+
+        let input = Matrix::from_iter(2, 2, vec![0.1, 0.2, 0.3, 0.4]);
+        let output = Matrix::from_iter(2, 1, vec![0.5, 0.6]);
+
+        let cost = nn.cost(&input, &output);
+
+        assert_eq!(cost, 0.0997167628239912);
+    }
+
+    #[test]
+    fn test_finite_diff() {
+        let mut nn = NeuralNetwork::new(&[2, 3, 1]);
+        nn.weight[0] = Matrix::from_iter(2, 3, vec![0.1, 0.2, 0.3, 0.4, 0.5, 0.6]);
+        nn.weight[1] = Matrix::from_iter(3, 1, vec![0.7, 0.8, 0.9]);
+        nn.bias[0] = Matrix::from_iter(1, 3, vec![0.1, 0.2, 0.3]);
+        nn.bias[1] = Matrix::from_iter(1, 1, vec![0.4]);
+
+        let input = Matrix::from_iter(2, 2, vec![0.1, 0.2, 0.3, 0.4]);
+        let output = Matrix::from_iter(2, 1, vec![0.5, 0.6]);
+
+        let mut gradient = NeuralNetwork::new(&[2, 3, 1]);
+        let eps = 0.0001;
+        nn.finite_diff(&mut gradient, &eps, &input, &output);
+
+        assert_eq!(
+            gradient.weight[0],
+            Matrix::from_iter(
+                2,
+                3,
+                vec![
+                    0.002338431382142847,
+                    0.0026023290392029885,
+                    0.0028172072125132175,
+                    0.0036226984256870765,
+                    0.004037243530596868,
+                    0.004379096906204083
+                ]
+            )
+        );
+        assert_eq!(
+            gradient.weight[1],
+            Matrix::from_iter(
+                3,
+                1,
+                vec![
+                    0.04148976830056772,
+                    0.044146948279810694,
+                    0.046728225587544525
+                ]
+            )
+        );
+        assert_eq!(
+            gradient.bias[0],
+            Matrix::from_iter(
+                1,
+                3,
+                vec![
+                    0.012842612617247617,
+                    0.014349051058182294,
+                    0.01561876383046612
+                ]
+            )
+        );
+        assert_eq!(
+            gradient.bias[1],
+            Matrix::from_iter(1, 1, vec![0.07441508547284537])
+        );
+    }
+
+    #[test]
+    fn test_learn() {
+        let mut nn = NeuralNetwork::new(&[2, 3, 1]);
+        nn.weight[0] = Matrix::from_iter(2, 3, vec![0.1, 0.2, 0.3, 0.4, 0.5, 0.6]);
+        nn.weight[1] = Matrix::from_iter(3, 1, vec![0.7, 0.8, 0.9]);
+        nn.bias[0] = Matrix::from_iter(1, 3, vec![0.1, 0.2, 0.3]);
+        nn.bias[1] = Matrix::from_iter(1, 1, vec![0.4]);
+        nn.activation[0] = Matrix::from_iter(1, 2, vec![0.5, 0.6]);
+
+        let mut gradient = NeuralNetwork::new(&[2, 3, 1]);
+        gradient.weight[0] = Matrix::from_iter(2, 3, vec![0.1, 0.2, 0.3, 0.4, 0.5, 0.6]);
+        gradient.weight[1] = Matrix::from_iter(3, 1, vec![0.7, 0.8, 0.9]);
+        gradient.bias[0] = Matrix::from_iter(1, 3, vec![0.1, 0.2, 0.3]);
+        gradient.bias[1] = Matrix::from_iter(1, 1, vec![0.4]);
+
+        nn.learn(&mut gradient, &0.1);
+
+        assert_eq!(
+            nn.weight[0],
+            Matrix::from_iter(2, 3, vec![0.090, 0.180, 0.270, 0.360, 0.450, 0.540])
+        );
+        assert_eq!(
+            nn.weight[1],
+            Matrix::from_iter(3, 1, vec![0.630, 0.720, 0.810])
+        );
+        assert_eq!(nn.bias[0], Matrix::from_iter(1, 3, vec![0.1, 0.2, 0.3]));
+        assert_eq!(nn.bias[1], Matrix::from_iter(1, 1, vec![0.4]));
+    }
+
+    #[test]
+    fn test_nn_test() {
+        let mut nn = NeuralNetwork::new(&[2, 3, 1]);
+        nn.weight[0] = Matrix::from_iter(2, 3, vec![0.1, 0.2, 0.3, 0.4, 0.5, 0.6]);
+        nn.weight[1] = Matrix::from_iter(3, 1, vec![0.7, 0.8, 0.9]);
+        nn.bias[0] = Matrix::from_iter(1, 3, vec![0.1, 0.2, 0.3]);
+        nn.bias[1] = Matrix::from_iter(1, 1, vec![0.4]);
+
+        let input = Matrix::from_iter(1, 2, vec![0.5, 0.6]);
+        let output = nn.test(&input);
+
+        assert_eq!(output, Matrix::from_iter(1, 1, vec![0.8761885526812198]));
+    }
+}
